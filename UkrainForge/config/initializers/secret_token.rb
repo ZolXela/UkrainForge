0b0f1cbe02ea8fea751e7de0d0ebbf33266a8464
Alexander Manuhin <amanuhin@adyax.com>
2013-05-16 18:24:10 +0300

    Initial commit

---
+++ UkrainForge/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UkrainForge::Application.config.secret_token = '85453a8c5d8ded824e54468ca585b088886928ba0e4b4646c868748c48eaf3da38ad1467cf10dccce35dd3c968ceee14f59fd94b7b10f6ddef59aa9935174cc7'
